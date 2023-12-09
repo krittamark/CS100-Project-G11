174df104d9916066688ae82b3f7d27bb8a3c621b
krittamark-u <krittamet.dam@dome.tu.ac.th>
2023-12-09 11:01:49 +0700

    Update from Mark#1180

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', passportRouter);
